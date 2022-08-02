a780f6e593b813119a28f7c7f555b7bf34d7c4d4
mafiti91 <ppivankov@gmail.com>
2022-08-02 11:50:20 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -37,8 +37,33 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+let arrSymbMorse = Object.keys(MORSE_TABLE);
+
 function decode(expr) {
-    // write your solution here
+    let arrStr = expr.match(/.{1,10}/g);
+    let arrRes = [];
+    for(elem in arrStr) {
+        let index = 0;
+        while(arrStr[elem][index] === '0') {
+            arrStr[elem] = arrStr[elem].replace(/^0+/, "");    
+            index +=1;
+        } 
+        arrRes.push(arrStr[elem].match(/.{1,2}/g));     
+    };
+    for (elem in arrRes) {
+        for (i = 0; i < arrRes[elem].length; i ++) {
+            if(arrRes[elem][i] === '10') arrRes[elem][i] = '.'
+            else if (arrRes[elem][i] === '11') arrRes[elem][i] = '-'
+                 else  if (arrRes[elem][i] === '**') arrRes[elem] = [' '];
+        };    
+    };
+    for (key in arrRes) {
+        arrRes[key] = arrRes[key].join('');
+        for (i = 0; i < arrSymbMorse.length; i ++) {
+            if (arrRes[key] === arrSymbMorse[i]) arrRes[key] = MORSE_TABLE[arrSymbMorse[i]];
+        };
+    };
+    return arrRes.join('');
 }
 
 module.exports = {
